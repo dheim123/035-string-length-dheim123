47ccc79927c11cd194ccc26b24a70b52d51299e1
dheim123 <dheim@midland.k12.ia.us>
2025-10-15 15:42:15 +0000

    2

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var quote = "...ask not what your country can do for you — ask what you can do for your country.";
 
 //Add code below
-var length = quotelength;
-var twentySecondLetter = quotelength[21];
+var quotelength = quote.length;
+var twentySecondLetter = quote[21];
 var output = twentySecondLetter;
 
 document.getElementById("result").innerHTML = output;
